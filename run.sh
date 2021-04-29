4057197d423d9ea8d1e6c88a018e5c7f6413431a
saidaltindis <sfahri.altindis@tedu.edu.tr>
2021-04-29 23:24:43 +0300

    changes for 256 image size

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nohup python main.py --mode train --num_domains 2 --w_hpf 1 --lambda_reg 1 --lambda_sty 1 --lambda_ds 1 --lambda_cyc 1 --train_img_dir data/celeba_hq/train --val_img_dir data/celeba_hq/val &> log.out &
